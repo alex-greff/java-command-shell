9f0d7522c8594cccdb41213d113f1343f472249f
Alexander Greff <34700852+alex-greff@users.noreply.github.com>
2018-06-16 00:39:03 +0000

    Add unitTests package. Added utilities package and created Parser class.

---
+++ src/utilities/Parser.java
@@ -0,0 +1,16 @@
+package utilities;
+
+import containers.*;
+
+public class Parser {
+  /**
+   * Parses the given input and returns a CommandArgs instance with the parsed
+   * information 
+   * @param input The user input string
+   * @return Returns a CommandArgs instance with the parsed user input
+   */
+  public static CommandArgs ParseUserInput(String input) {
+    
+    return null;
+  }
+}
