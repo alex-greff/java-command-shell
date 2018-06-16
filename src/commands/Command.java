319be5fdcdeefe4ae30469191c4057490c2005fd
Alexander <30787801+Alxander64@users.noreply.github.com>
2018-06-16 02:43:48 +0000

    More base code on CommandManager class and Command Interface

--- src/commands/Command.java
+++ src/commands/Command.java
@@ -1,5 +1,8 @@
 package commands;
 
 public interface Command {
+
   String execute();
+
+  int getArgNum();
 }
--- src/utilities/CommandManager.java
+++ src/utilities/CommandManager.java
@@ -1,5 +1,14 @@
 package utilities;
 
+import containers.*;
+import commands.*;
+import java.util.HashMap;
+
 public class CommandManager {
 
+  private static HashMap<String, Command> cmdList = new HashMap<>();
+
+  public static void attemptCommand(CommandArgs cArgs) {
+
+  }
 }
