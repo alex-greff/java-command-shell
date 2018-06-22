af297d998de8bbcb83443e7711478771fd84805a
Anton Kaminsky <antonwizkam@gmail.com>
2018-06-22 15:12:44 +0000

    Complete path class and add malformed path exception

---
+++ src/filesystem/MalformedPathException.java
@@ -0,0 +1,10 @@
+package filesystem;
+
+/**
+ * Thrown when a bad path is given
+ *
+ * @author anton
+ */
+public class MalformedPathException extends Throwable {
+
+}
--- src/filesystem/Path.java
+++ src/filesystem/Path.java
@@ -1,8 +1,50 @@
 package filesystem;
 
+import java.util.ArrayList;
+
 /**
  * Represents a path in the filesystem
+ *
+ * @author anton
  */
 public class Path {
 
+  private ArrayList<String> tokens = new ArrayList<>();
+
+  /**
+   * Creates a new path given a path string
+   *
+   * @param pathString A string representing a path
+   */
+  public Path(String pathString) throws MalformedPathException {
+    // if it's an absolute path add the root dir to the token list
+    if (pathString.startsWith("/")) {
+      tokens.add("/");
+      pathString = pathString.substring(1);
+    }
+    for (String segment : pathString.split("/")) {
+      if (!segment.isEmpty()) {
+        tokens.add(segment);
+      } else {
+        // can't have a slash after another slash or empty space
+        throw new MalformedPathException();
+      }
+    }
+  }
+
+  /**
+   * Returns the next path token and removes it
+   * @return the next path token
+   */
+  public String getNextToken() {
+    return tokens.remove(0);
+  }
+
+  /**
+   * Query empty status of the tokens
+   * @return True if no more tokens false otherwise
+   */
+  public boolean isEmpty() {
+    return this.tokens.isEmpty();
+  }
 }
