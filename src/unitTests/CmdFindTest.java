624ed76bc1285f6a589d84252e039fe0b97ad9a0
Alexander Greff <34700852+alex-greff@users.noreply.github.com>
2018-06-25 04:05:08 +0000

    Added test file CmdFindTest

---
+++ src/unitTests/CmdFindTest.java
@@ -0,0 +1,14 @@
+package unitTests;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import commands.CmdEcho;
+import containers.CommandArgs;
+import utilities.Command;
+
+public class CmdFindTest {
+  @Test
+  public void testExecute1() {
+      // TODO: make some tests
+  }
+}
