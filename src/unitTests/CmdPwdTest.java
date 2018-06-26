97c830cfafa058512ef2a55f22f286b355b8b879
Alexander <30787801+Alxander64@users.noreply.github.com>
2018-06-26 23:00:23 +0000

    Test for pwd

---
+++ src/unitTests/CmdPwdTest.java
@@ -0,0 +1,25 @@
+package unitTests;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import filesystem.FileSystem;
+import org.junit.Test;
+import commands.CmdPwd;
+import containers.CommandArgs;
+import utilities.Command;
+
+public class CmdPwdTest {
+  @Test
+  public void textExecute1() {
+    CommandArgs args = new CommandArgs("pwd");
+
+    Command cmd = new CmdPwd();
+    String out_actual = cmd.execute(args);
+
+    FileSystem FS = FileSystem.getInstance();
+
+    System.out.println(out_actual);
+
+    assertEquals(FS.getWorkingDirPath(), out_actual);
+  }
+}
