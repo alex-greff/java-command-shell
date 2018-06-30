b8cd1339e87122e542deb1222818bbbd372a3609
Alexander <30787801+Alxander64@users.noreply.github.com>
2018-06-30 06:07:48 +0000

    Test file for cd

--- src/unitTests/CmdCdTest.java
+++ src/unitTests/CmdCdTest.java
@@ -19,7 +19,7 @@ public class CmdCdTest {
   private Command cmd = new CmdCd();
 
   @Test
-  public void testDirInWorkingDir() throws FileAlreadyExistsException {
+  public void testChildDir() throws FileAlreadyExistsException {
     String argParam[] = {"testDir"};
     CommandArgs args = new CommandArgs("cd", argParam);
 
@@ -27,7 +27,40 @@ public class CmdCdTest {
     FS.getRoot().createAndAddNewDir("testDir");
 
     ExitCode exitVal = cmd.execute(args, out, errOut);
+    assertEquals(exitVal, ExitCode.SUCCESS);
+    assertEquals(FS.getWorkingDirPath(), "/testDir/");
+  }
+
+  @Test
+  public void testCurrentDir() {
+    String argParam[] = {"."};
+    CommandArgs args = new CommandArgs("cd", argParam);
+
+    FileSystem FS = FileSystem.getInstance();
+
+    cmd.execute(args, out, errOut);
+    ExitCode exitVal = cmd.execute(args, out, errOut);
 
     assertEquals(exitVal, ExitCode.SUCCESS);
+    assertEquals(FS.getWorkingDirPath(), "/testDir/");
   }
+
+  @Test
+  public void testParentDir() throws FileAlreadyExistsException {
+    String argParam1[] = {"testDir"};
+    CommandArgs args1 = new CommandArgs("cd", argParam1);
+
+    String argParam2[] = {".."};
+    CommandArgs args2 = new CommandArgs("cd", argParam2);
+
+    FileSystem FS = FileSystem.getInstance();
+    FS.getRoot().createAndAddNewDir("testDir");
+
+    cmd.execute(args1, out, errOut);
+    ExitCode exitVal = cmd.execute(args2, out, errOut);
+
+    assertEquals(exitVal, ExitCode.SUCCESS);
+    assertEquals(FS.getWorkingDirPath(), "/");
+  }
+
 }
