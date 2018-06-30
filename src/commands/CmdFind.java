98636cc7101190c9283f6f4d5a352df8449d824b
Alexander <30787801+Alxander64@users.noreply.github.com>
2018-06-30 00:50:55 +0000

    Honor Code

--- src/commands/CmdFind.java
+++ src/commands/CmdFind.java
@@ -1,3 +1,32 @@
+// **********************************************************
+// Assignment2:
+// Student1:
+// UTORID user_name: ursualex
+// UT Student #: 1004357199
+// Author: Alexander Ursu
+//
+// Student2:
+// UTORID user_name: greffal1
+// UT Student #: 1004254497
+// Author: Alexander Greff
+//
+// Student3:
+// UTORID user_name: sankarch
+// UT Student #: 1004174895
+// Author: Chedy Sankar
+//
+// Student4:
+// UTORID user_name: kamins42
+// UT Student #: 1004431992
+// Author: Anton Kaminsky
+//
+//
+// Honor Code: I pledge that this program represents my own
+// program code and that I have coded on my own. I received
+// help from no one in designing and debugging my program.
+// I have also read the plagiarism section in the course info
+// sheet of CSC B07 and understand the consequences.
+// *********************************************************
 package commands;
 
 import containers.CommandArgs;
@@ -90,8 +119,8 @@ public class CmdFind extends Command {
 
 
   /**
-   * Gets a set of all absolute paths to instances of files with the
-   * name "name"
+   * Gets a set of all absolute paths to instances of files with the name
+   * "name"
    *
    * @param dir The current directory
    * @param name The wanted file name
@@ -136,8 +165,8 @@ public class CmdFind extends Command {
   }
 
   /**
-   * Gets a set of all absolute paths to instances of directories with
-   * the name "name"
+   * Gets a set of all absolute paths to instances of directories with the name
+   * "name"
    *
    * @param dir The current directory
    * @param name The wanted directory name
@@ -172,8 +201,7 @@ public class CmdFind extends Command {
   }
 
   /**
-   * A helper checking if args is a valid CommandArgs instance for
-   * this command
+   * A helper checking if args is a valid CommandArgs instance for this command
    *
    * @param args The command arguments
    * @return Returns true iff args is a valid for this command
