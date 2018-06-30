19bc843c83e0db967a4c085e76c058a850adc67d
Alexander <30787801+Alxander64@users.noreply.github.com>
2018-06-30 06:32:34 +0000

    Started test file for cat

--- src/unitTests/CmdCatTest.java
+++ src/unitTests/CmdCatTest.java
@@ -2,15 +2,18 @@ package unitTests;
 
 import static org.junit.Assert.assertEquals;
 
-import commands.CmdCd;
+import commands.CmdCat;
 import containers.CommandArgs;
-import filesystem.FileAlreadyExistsException;
-import filesystem.FileSystem;
 import io.Console;
 import io.ErrorConsole;
 import org.junit.Test;
 import utilities.Command;
+import utilities.ExitCode;
 
 public class CmdCatTest {
+
+  private Console out = Console.getInstance();
+  private ErrorConsole errOut = ErrorConsole.getInstance();
+  private Command cmd = new CmdCat();
 
 }
