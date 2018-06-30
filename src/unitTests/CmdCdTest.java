c609d06bf652e60ac2aff373dc3fce69c7ec55b8
Alexander <30787801+Alxander64@users.noreply.github.com>
2018-06-30 06:29:15 +0000

    More tests for cd

--- src/unitTests/CmdCdTest.java
+++ src/unitTests/CmdCdTest.java
@@ -22,6 +22,7 @@ public class CmdCdTest {
   @Test
   public void testChildDir() throws FileAlreadyExistsException {
     FS.getRoot().createAndAddNewDir("testDir1");
+    FS.getRoot().createAndAddNewDir("testDir2");
 
     String argParam[] = {"testDir1"};
     CommandArgs args = new CommandArgs("cd", argParam);
@@ -32,7 +33,9 @@ public class CmdCdTest {
   }
 
   @Test
-  public void testCurrentDir() {
+  public void testCurrentDir() throws FileAlreadyExistsException {
+    FS.getWorkingDir().createAndAddNewDir("testDir3");
+
     String argParam[] = {"."};
     CommandArgs args = new CommandArgs("cd", argParam);
 
@@ -49,6 +52,26 @@ public class CmdCdTest {
     ExitCode exitVal = cmd.execute(args, out, errOut);
     assertEquals(exitVal, ExitCode.SUCCESS);
     assertEquals(FS.getWorkingDirPath(), "/");
+  }
+
+  @Test
+  public void testAbsolutePathToDir() {
+    String argParam[] = {"/testDir1/testDir3"};
+    CommandArgs args = new CommandArgs("cd", argParam);
+
+    ExitCode exitVal = cmd.execute(args, out, errOut);
+    assertEquals(exitVal, ExitCode.SUCCESS);
+    assertEquals(FS.getWorkingDirPath(), "/testDir1/testDir3");
+  }
+
+  @Test
+  public void testRelativePathToDir() {
+    String argParam[] = {"../../testDir2"};
+    CommandArgs args = new CommandArgs("cd", argParam);
+
+    ExitCode exitVal = cmd.execute(args, out, errOut);
+    assertEquals(exitVal, ExitCode.SUCCESS);
+    assertEquals(FS.getWorkingDirPath(), "/testDir2");
   }
 
 }
