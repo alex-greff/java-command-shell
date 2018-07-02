19556ef2ec6568d0212fb096eb9441b14c481f9b
Alexander <30787801+Alxander64@users.noreply.github.com>
2018-07-02 05:39:45 +0000

    extra test case for cat

--- src/unitTests/CmdCatTest.java
+++ src/unitTests/CmdCatTest.java
@@ -19,7 +19,7 @@ public class CmdCatTest {
   private FileSystem FS = FileSystem.getInstance();
 
   @Test
-  public void testOneFile() throws FileAlreadyExistsException {
+  public void testFileWithOneLine() throws FileAlreadyExistsException {
     File file1 = new File("testFile1", "hello");
     FS.getRoot().addFile(file1);
 
@@ -32,7 +32,7 @@ public class CmdCatTest {
   }
 
   @Test
-  public void testMultipleFiles() throws FileAlreadyExistsException {
+  public void testMultipleFilesWithOneLine() throws FileAlreadyExistsException {
     File file2 = new File("testFile2", "world");
     FS.getRoot().addFile(file2);
 
@@ -44,4 +44,18 @@ public class CmdCatTest {
     assertEquals(testOut.getAllWritesAsString(), "hello\n\n\nworld\n");
   }
 
+  @Test
+  public void testFileWithMultipleLines() throws FileAlreadyExistsException {
+    File file3 = new File("testFile3", "hello\nworld\nthis\n"
+                                                    + "is\na\ntest");
+    FS.getRoot().addFile(file3);
+
+    String argParam[] = {"testFile3"};
+    CommandArgs args = new CommandArgs("cat", argParam);
+
+    ExitCode exitVal = cmd.execute(args, testOut, testErrOut);
+    assertEquals(exitVal, ExitCode.SUCCESS);
+    assertEquals(testOut.getAllWritesAsString(),"hello\nworld\nthis\n"
+                                                    + "is\na\ntest\n");
+  }
 }
