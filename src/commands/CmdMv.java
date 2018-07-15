8c6dbba7064b048f1cb6bd65f45079538fd5fffe
Alexander <30787801+Alxander64@users.noreply.github.com>
2018-07-15 19:43:07 +0000

    work on mv command

--- src/commands/CmdMv.java
+++ src/commands/CmdMv.java
@@ -38,6 +38,7 @@ import filesystem.Directory;
 import filesystem.FSElement;
 import filesystem.FSElementAlreadyExistsException;
 import filesystem.FSElementNotFoundException;
+import filesystem.File;
 import filesystem.FileSystem;
 import filesystem.MalformedPathException;
 import filesystem.Path;
@@ -109,43 +110,32 @@ public class CmdMv extends Command {
    */
   @Override
   public ExitCode execute(CommandArgs args, Writable out, Writable errOut) {
-    Path oldPath = null;
-    Path newPath = null;
+    Path oldPath, newPath;
 
     try {
       oldPath = new Path(args.getCommandParameters()[0]);
       newPath = new Path(args.getCommandParameters()[1]);
     } catch (MalformedPathException e) {
       errOut.write("Invalid path(s) given");
+      return ExitCode.FAILURE;
     }
 
-    FSElement oldElement = getElement(oldPath);
-    FSElement newElement = getElement(newPath);
+    Path oldParentPath = oldPath;
+    oldParentPath.removeLast();
+    Path newParentPath = newPath;
+    newParentPath.removeLast();
 
-    return ExitCode.SUCCESS;
-  }
-
-  /**
-   *
-   * @param wantedPath
-   * @return
-   */
-  private FSElement getElement(Path wantedPath) {
-    FSElement wantedElement = null;
+    Directory oldParent, newParent;
 
     try {
-      wantedElement = fileSystem.getFileByPath(wantedPath);
+      oldParent = fileSystem.getDirByPath(oldParentPath);
+      newParent = fileSystem.getDirByPath(newParentPath);
     } catch (MalformedPathException | FSElementNotFoundException e) {
+      errOut.write("Source not found and/or or path leading to destination not found");
+      return ExitCode.FAILURE;
     }
 
-    if (wantedElement == null) {
-      try {
-        wantedElement = fileSystem.getDirByPath(wantedPath);
-      } catch (MalformedPathException | FSElementNotFoundException e) {
-      }
-    }
-
-    return wantedElement;
+    return ExitCode.SUCCESS;
   }
 
   /**
