b18a470530f85f9a77ca501c7ec843c973dac22b
Anton Kaminsky <antonwizkam@gmail.com>
2018-07-18 19:28:21 +0000

    Add more mv tests

--- src/unitTests/CmdMvTest.java
+++ src/unitTests/CmdMvTest.java
@@ -46,7 +46,7 @@ public class CmdMvTest {
                                        new String[]{"inside_test", "test"});
     ExitCode mvExit = mvCmd.execute(args, tc, tc_err);
     // this should run successfully
-    assertEquals(mvExit, ExitCode.SUCCESS);
+    assertEquals(ExitCode.SUCCESS, mvExit);
     // there should no longer be inside_test as a child of root
     assertFalse(root.containsDir("inside_test"));
     // inside_test should now be inside test
@@ -54,19 +54,95 @@ public class CmdMvTest {
   }
 
   @Test
-  public void test_moving_directory_to_a_non_existent_directory()
+  public void test_moving_directory_to_a_non_existent_directory_in_same_tree()
       throws FSElementAlreadyExistsException {
-      Directory root = fs.getRoot();
-      root.createAndAddNewDir("inside_test");
-      // mv inside_test test
-      CommandArgs args = new CommandArgs("mv",
-                                         new String[]{"inside_test", "test"});
-      ExitCode mvExit = mvCmd.execute(args, tc, tc_err);
-      // this should run successfully
-      assertEquals(mvExit, ExitCode.SUCCESS);
-      // there should no longer be inside_test as a child of root
-      assertFalse(root.containsDir("inside_test"));
-      // there should now only be test inside of root
-      assertTrue(root.containsDir("test"));
+    Directory root = fs.getRoot();
+    root.createAndAddNewDir("inside_test");
+    // mv inside_test test
+    CommandArgs args = new CommandArgs("mv",
+                                       new String[]{"inside_test", "test"});
+    ExitCode mvExit = mvCmd.execute(args, tc, tc_err);
+    // this should run successfully
+    assertEquals(ExitCode.SUCCESS, mvExit);
+    // there should no longer be inside_test as a child of root
+    assertFalse(root.containsDir("inside_test"));
+    // there should now only be test inside of root
+    assertTrue(root.containsDir("test"));
+  }
+
+  @Test
+  public void test_moving_directory_to_itself()
+      throws FSElementAlreadyExistsException {
+    Directory root = fs.getRoot();
+    root.createAndAddNewDir("test");
+    // mv inside_test test
+    CommandArgs args = new CommandArgs("mv",
+                                       new String[]{"test", "test"});
+    ExitCode mvExit = mvCmd.execute(args, tc, tc_err);
+    // this should fail
+    assertEquals(ExitCode.FAILURE, mvExit);
+    assertEquals("Cannot move element to its child or itself",
+                 tc_err.getLastWrite());
+    // nothing should happen to the directory
+    assertTrue(root.containsDir("test"));
+  }
+
+  @Test
+  public void test_moving_directory_to_non_existing_directory_in_diff_tree()
+      throws FSElementAlreadyExistsException {
+    Directory root = fs.getRoot();
+    root.createAndAddNewDir("getinme");
+    root.createAndAddNewDir("seeyalater");
+    // mv inside_test test
+    CommandArgs args =
+        new CommandArgs("mv",
+                        new String[]{"seeyalater", "getinme/heythere"});
+    ExitCode mvExit = mvCmd.execute(args, tc, tc_err);
+    // this should run successfully
+    assertEquals(ExitCode.SUCCESS, mvExit);
+    // there should no longer be seeyalater as a child of root
+    assertFalse(root.containsDir("seeyalater"));
+    // there should now be heythere inside getinme
+    assertTrue(root.getDirByName("getinme").containsDir("heythere"));
+  }
+
+  @Test
+  public void test_renaming_non_empty_directory()
+      throws FSElementAlreadyExistsException {
+    Directory root = fs.getRoot();
+    Directory daddy = root.createAndAddNewDir("daddy");
+    daddy.createAndAddNewDir("kid1");
+    daddy.createAndAddNewDir("kid2");
+    // mv daddy mommy
+    CommandArgs args = new CommandArgs("mv", new String[]{"daddy", "mommy"});
+    ExitCode mvExit = mvCmd.execute(args, tc, tc_err);
+    // this should run successfully
+    assertEquals(ExitCode.SUCCESS, mvExit);
+    // there should no longer be daddy as a child of root
+    assertFalse(root.containsDir("daddy"));
+    // there should now only be mommy inside of root
+    assertTrue(root.containsDir("mommy"));
+    // make sure the kids don't get aborted
+    assertTrue(root.getDirByName("mommy").containsDir("kid1"));
+    assertTrue(root.getDirByName("mommy").containsDir("kid2"));
+  }
+
+  @Test
+  public void test_moving_non_root_child()
+      throws FSElementAlreadyExistsException {
+    Directory root = fs.getRoot();
+    Directory daddy = root.createAndAddNewDir("daddy");
+    daddy.createAndAddNewDir("kid1");
+    daddy.createAndAddNewDir("kid2");
+    // mv kid1 kid2
+    CommandArgs args =
+        new CommandArgs("mv", new String[]{"daddy/kid1", "daddy/kid2"});
+    ExitCode mvExit = mvCmd.execute(args, tc, tc_err);
+    // this should succeed
+    assertEquals(ExitCode.SUCCESS, mvExit);
+    // daddy kid 1 should no longer exist
+    assertFalse(daddy.containsDir("kid1"));
+    // kid2 should exist and contain kid 1
+    assertTrue(daddy.getDirByName("kid2").containsDir("kid1"));
   }
 }
